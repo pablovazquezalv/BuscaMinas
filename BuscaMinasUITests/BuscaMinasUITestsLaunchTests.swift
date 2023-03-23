81e2b332820f1854e7b2e34b57cd886eb58c90c3
Pablo <Pablo>
2023-03-23 11:29:23 -0600

    Initial Commit

---
+++ BuscaMinasUITests/BuscaMinasUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BuscaMinasUITestsLaunchTests.swift
+//  BuscaMinasUITests
+//
+//  Created by imac on 15/03/23.
+//
+
+import XCTest
+
+final class BuscaMinasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
